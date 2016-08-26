9cf655b2a164672ed6c2b2b2902882d220feffd2
FAN, Kevin <kfan20@knox.nsw.edu.au>
2016-08-26 17:51:31 +1000

    index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+var readline = require('readline');
+
+var readlineThing = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readlineThing.question("What is your name?", function(answer) {
+  console.log("Hi there " + answer + ", welcome to the game!")
+  readlineThing.close();
+})
+
+var Player = {
+  name: "Khan",
+  race: "Human",
+  class: "Warrior",
+  gender: "Male",
+  health: "20",
+  skill: {
+    1: null,
+    2: null,
+    3: null
+  },
+  eat: function() {
+    if (Player.health == 19) {
+      health = 20;
+    } else if (Player.health == 20) {
+      console.log("You are on full health. Stop eating.")
+    } else {
+      health = health + 2
+    }
+  }
+}
+
+console.log(Player)
+
+var FinalBoss = {
+  name: "Alan the Terrible",
+  health: 100,
+  skill: {
+    attack: function() {
+      console.log("Get rekt scrub, mlg 360 no scope");
+      Player.health = Player.health - 10;
+    },
+    taunt: function() {
+      console.log("Go home noob");
+    }
+  }
+}
+
+console.log(FinalBoss);

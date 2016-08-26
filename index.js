120e4722105556be5c3e87fb981ca76b085ae5f2
FAN, Kevin <kfan20@knox.nsw.edu.au>
2016-08-26 18:50:52 +1000

    added classes

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-var readline = require('readline');
+var game = {
+  start: function() {
+    var readline = require('readline');
 
-var readlineThing = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+    var readlineThing = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
-readlineThing.question("What is your name?", function(answer) {
-  console.log("Hi there " + answer + ", welcome to the game!")
-  readlineThing.close();
-})
+    readlineThing.question("What is your name? ", function(answer) {
+      console.log("Hi there " + answer + ", welcome to the game!")
+      readlineThing.close();
+    })
+  },
+  restart: function() {
+    game.start();
+  }
+}
 
 var Player = {
   name: "Khan",
@@ -32,8 +39,6 @@ var Player = {
   }
 }
 
-console.log(Player)
-
 var FinalBoss = {
   name: "Alan the Terrible",
   health: 100,
@@ -48,4 +53,64 @@ var FinalBoss = {
   }
 }
 
-console.log(FinalBoss);
+function chooseClass(answer) {
+  Player.class = answer;
+  if (Player.class == "Mage") {
+    Player.skill["1"] = function() {
+      console.log("Take this Motherf*cker!")
+      finalBoss.health = finalBoss.health - 10;
+    }
+    Player.skill["2"] = function() {
+      console.log("Eat fire");
+      finalBoss.health = finalBoss.health - 20;
+    }
+    Player.skill["3"] = function() {
+      console.log("I haz more health")
+      if (Player.health > 10) {
+        Player.health = 20;
+      } else {
+        Player.health = Player.health + 8;
+      }
+    }
+  } else if (Player.class == "Warrior") {
+      Player.skill["1"] = function() {
+        console.log("Take this Motherf*cker!")
+        finalBoss.health = finalBoss.health - 7;
+      }
+      Player.skill["2"] = function() {
+        console.log("Stab Stab Stab");
+        finalBoss.health = finalBoss.health - 12;
+      }
+      Player.skill["3"] = function() {
+        console.log("Boom!")
+        finalBoss.health = finalBoss.health - 20
+      }
+    } else if (Player.class == "Thief") {
+        Player.skill["1"] = function() {
+          console.log("Take this Motherf*cker!")
+          finalBoss.health = finalBoss.health - 7;
+        }
+        Player.skill["2"] = function() {
+          console.log("Shanking Intensifies");
+          finalBoss.health = finalBoss.health - 9;
+        }
+        Player.skill["3"] = function() {
+          console.log("Eat arrow")
+          finalBoss.health = finalBoss.health - 5
+        }
+      } else if (Player.class == "Giant") {
+          Player.skill["1"] = function() {
+            console.log("Stomp")
+            finalBoss.health = finalBoss.health - 4;
+          }
+          Player.skill["2"] = function() {
+            console.log("Crushing blow");
+            finalBoss.health = finalBoss.health - 10;
+          }
+          Player.skill["3"] = function() {
+            console.log("Sonic Clap")
+            finalBoss.health = finalBoss.health - 6
+          }
+        }
+
+game.start();
